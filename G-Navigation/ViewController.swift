95ac2d0a08f48a4c1177a43d8e2c2698a66d48e9
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-01-19 18:35:26 +0300

    Google Map pod installed.

---
+++ G-Navigation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  G-Navigation
+//
+//  Created by Mutlu Çalkan on 19.01.2023.
+//
+
+import UIKit
+
+//Google API KEY: AIzaSyDkBejn3YScabgiQQT3Clp8G-abF055sRE
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
