d174f13d86715ac356d44c5a25a318b563f2ebd5
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-01-20 13:00:54 +0300

    GoogleMaps added.

--- G-Navigation/ViewController.swift
+++ G-Navigation/ViewController.swift
@@ -6,16 +6,62 @@
 //
 
 import UIKit
-
-//Google API KEY: AIzaSyDkBejn3YScabgiQQT3Clp8G-abF055sRE
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //GoogleMapsLicense
+        print("License: \n\n\(GMSServices.openSourceLicenseInfo())")
+        
+        //CoreLocation
+        configureLocationManager()
+        
+    }
+    
+    private func addGoogleMaps(lat: CLLocationDegrees, long: CLLocationDegrees){
+        let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 13.0)
+        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
+        view.addSubview(mapView)
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+    
+    private func configureLocationManager(){
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        print("Start Updating Location")
     }
 
+    
+}
 
+extension ViewController: CLLocationManagerDelegate{
+    
+ func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     
+     guard let location = locations.first else {
+         print("Error while getting first location!")
+         return
+     }
+     
+     let coordinate = location.coordinate
+     print("Coordinates fetched successfully.")
+     
+     addGoogleMaps(lat: coordinate.latitude, long: coordinate.longitude)
+ }
+    
 }
 
