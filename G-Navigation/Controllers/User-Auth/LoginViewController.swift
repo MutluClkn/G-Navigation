c30e35e8cee611a9eca25cda6d87a97fa543c5da
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-04-17 09:49:36 +0300

    Auth features added. User can register and login via Firebase.

--- G-Navigation/Controllers/User-Auth/LoginViewController.swift
+++ G-Navigation/Controllers/User-Auth/LoginViewController.swift
@@ -5,24 +5,45 @@
 //  Created by Mutlu Çalkan on 23.03.2023.
 //
 
+
+//MARK: - Libraries
 import UIKit
 import FirebaseAuth
 
+//MARK: - LoginViewController
 final class LoginViewController: BaseViewController {
     
-    @IBOutlet weak var loginView: UIView!
+    //-----------------------------
+    //MARK: - Properties
+    //-----------------------------
     
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    //-----------------------------
+    //MARK: - Lifecycle
+    //-----------------------------
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //View layer
         loginView.layer.masksToBounds = true
         loginView.layer.cornerRadius = loginView.frame.size.height * 0.1
         
+        //Hide Keyboard
         closeKeyboard()
     }
     
+    
+    //-----------------------------
+    //MARK: - Actions
+    //-----------------------------
+    
+    //Login Button Pressed
     @IBAction func loginButtonDidPress(_ sender: UIButton) {
-        
+
         let alertController = UIAlertController(title: nil, message: "Please wait\n\n", preferredStyle: .alert)
         let spinnerIndicator = UIActivityIndicatorView(style: .large)
         spinnerIndicator.center = CGPoint(x: 135.0, y: 65.5)
@@ -31,7 +52,24 @@ final class LoginViewController: BaseViewController {
         alertController.view.addSubview(spinnerIndicator)
         self.present(alertController, animated: false, completion: nil)
         
-        self.performSegue(withIdentifier: SegueConstants.loginToStartVC, sender: nil)
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+            if let error {
+                alertController.dismiss(animated: true)
+                strongSelf.alertMessage(alertTitle: "Error", alertMesssage: error.localizedDescription)
+            }
+            else {
+                alertController.dismiss(animated: true)
+                self?.performSegue(withIdentifier: SegueConstants.loginToStartVC, sender: nil)
+            }
+        }
     }
     
+    //Register Button Pressed to navigate register screen
+    @IBAction func registerButtonDidPress(_ sender: UIButton) {
+        self.performSegue(withIdentifier: SegueConstants.loginToRegister, sender: nil)
+    }
 }
--- G-Navigation/Controllers/User-Auth/RegisterViewController.swift
+++ G-Navigation/Controllers/User-Auth/RegisterViewController.swift
@@ -5,21 +5,36 @@
 //  Created by Mutlu Çalkan on 29.03.2023.
 //
 
+//MARK: - Libraries
 import UIKit
 import FirebaseAuth
 
+//MARK: - RegisterViewController
 final class RegisterViewController: BaseViewController {
 
+    //-----------------------------
+    //MARK: - Properties
+    //-----------------------------
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var fullNameTextField: UITextField!
     
+    
+    //-----------------------------
+    //MARK: - Lifecycle
+    //-----------------------------
     
     override func viewDidLoad() {
         super.viewDidLoad()
         closeKeyboard()
     }
     
+    //-----------------------------
+    //MARK: - Actions
+    //-----------------------------
+    
+    //Register Button Pressed
     @IBAction func registerButtonDidPress(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text, let fullName = fullNameTextField.text {
             Auth.auth().createUser(withEmail: email, password: password){_, error in
